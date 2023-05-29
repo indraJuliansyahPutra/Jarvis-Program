501ec7b8e2736a22cab3e7e37e09b56dccc287b8
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2023-05-29 12:53:00 +0700

    Buat Command

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,9 @@
+import datetime
 import pyttsx3
 import speech_recognition as sr
+import os
+import cv2
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -13,7 +17,7 @@ def speak(audio):
     engine.runAndWait()
     
 # To convert voice into text
-def takeCommand():
+def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening....")
@@ -30,6 +34,45 @@ def takeCommand():
         return "none"
     return query
 
+
+#to wish
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    
+    if hour >= 0 and hour <= 12:
+        speak('Good Morning')
+    elif hour > 12 and hour <= 18:
+        speak('Good Afternoon')
+    else:
+        speak('Good Evening')
+    
+    speak('I am Jarvis sir, Please tell me how can i help you ?')
+
 if __name__ == '__main__':
-    takeCommand()
-    speak("Hello My Name Is Indra Juliansyah Putra")
+    speak("open command prompt please")
+    wish()
+    if 1:
+        
+        query = takecommand().lower()
+        
+        if "open notepad" in query:
+            npath = "C:\\Windows\\notepad.exe"
+            os.startfile(npath)
+            
+        elif "open wps" in query:
+            npath = "C:\\Users\\axioo\\AppData\\Local\\Kingsoft\\WPS Office\\ksolaunch.exe"
+            os.startfile(npath)
+            
+        elif "open command prompt" in query:
+            os.system("start cmd")
+            
+        elif "open camera" in query:
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow("webcam", img)
+                k = cv2.waitKey(50)
+                if k == 27:
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
